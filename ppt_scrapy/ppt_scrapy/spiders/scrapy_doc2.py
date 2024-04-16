4edb5f748fa20d3e79555743d6544454090ef048
Cheng-Hsun, Hsu <a10121555@gmail.com>
2024-04-16 19:47:24 +0800

    get the basic code

---
+++ ppt_scrapy/ppt_scrapy/spiders/scrapy_doc2.py
@@ -0,0 +1,22 @@
+import scrapy
+import re
+
+class ScrapyDocSpider(scrapy.Spider):
+    name = 'scrapy_doc'
+    allowed_domains = ['www.ptt.cc']
+    start_urls = ['https://www.ptt.cc/bbs/Gossiping/index.html']
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url, cookies={'over18': '1'}, callback=self.parse)
+    
+    def parse(self, response):
+        table = response.xpath("//div[@class='r-ent']")
+        for row in table:
+            title = row.xpath(".//div[@class='title']/a/text()").get()
+            url = row.xpath(".//div[@class='title']/a/@href").get()
+            match = re.search(r'\[(.*?)\]', title)
+            yield {
+                '列表名稱': match.group(1),
+                '網址': url,
+            }
