e5d8f49330fe4d6707068defeec914ce2773a21f
Cheng-Hsun, Hsu <a10121555@gmail.com>
2024-04-16 19:38:20 +0800

    successfully get the title and url

--- ppt_scrapy/ppt_scrapy/spiders/scrapy_doc.py
+++ ppt_scrapy/ppt_scrapy/spiders/scrapy_doc.py
@@ -4,7 +4,16 @@ import scrapy
 class ScrapyDocSpider(scrapy.Spider):
     name = 'scrapy_doc'
     allowed_domains = ['www.ptt.cc']
-    start_urls = ['http://www.ptt.cc/']
+    start_urls = ['https://www.ptt.cc/bbs/Gossiping/index.html']
 
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url, cookies={'over18': '1'}, callback=self.parse)
+    
     def parse(self, response):
-        pass
+        table = response.xpath("//div[@class='r-ent']")
+        for row in table:
+            title = row.xpath(".//div[@class='title']/a/text()").get()
+            url = row.xpath(".//div[@class='title']/a/@href").get()
+            print(title)
+            print(url)
