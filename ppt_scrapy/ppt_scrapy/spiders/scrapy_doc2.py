95e5469eefb0fa873b2c94b0fd136edbe348cdd0
Cheng-Hsun, Hsu <a10121555@gmail.com>
2024-04-16 20:17:58 +0800

    finish content

--- ppt_scrapy/ppt_scrapy/spiders/scrapy_doc2.py
+++ ppt_scrapy/ppt_scrapy/spiders/scrapy_doc2.py
@@ -12,21 +12,17 @@ class ScrapyDocSpider(scrapy.Spider):
             yield scrapy.Request(url, cookies={'over18': '1'}, callback=self.parse)
     
     def parse(self, response):
-        document_item = PptScrapyItem()
         table = response.xpath("//div[@class='r-ent']")
         for row in table:
-            # 標題
-            document_item['title'] = row.xpath(".//div[@class='title']/a/text()").get()
             url = row.xpath(".//div[@class='title']/a/@href").get()
-            match = re.search(r'\[(.*?)\]', title)
-            # 累別
-            document_item['category'] = match.group(1)
-            # 作者
-            document_item['author'] = row.xpath(".//div[@class='author']/text()").get()
-            # 發布時間
-            document_item['date'] = row.xpath(".//div[@class='date']/text()").get()
-
         yield scrapy.Request(url, cookies={'over18': '1'}, callback=self.extract_comment)
+
     def extract_comment(self, response):
-        # remember content
-        pass
+        document_item = PptScrapyItem()
+        title = response.xpath("//div[@id='main-content']/div[3]/span[2]/text()").get()
+        author = response.xpath("//div[@id='main-content']/div[1]/span[2]/text()").get()
+        match = re.search(r'\[(.*?)\]', title)
+        category = match.group(1)
+        date = response.xpath("//div[@id='main-content']/div[4]/span[2]/text()").get()
+        # Now deal with the comments
+
